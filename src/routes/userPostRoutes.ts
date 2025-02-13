89ce1b2bfcb8ee255b9bec348e41b5a56080d092
SukumarThalla <sukumar63044@gmail.com>
2025-02-13 17:20:30 +0530

    changed the post path from add-post to addPost

--- src/routes/userPostRoutes.ts
+++ src/routes/userPostRoutes.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { userPostController } from "../controllers/userPostControllers";
 const userPostRoutes = new Hono();
 
-userPostRoutes.post("/add-post", (c) => userPostController.postUser(c));
+userPostRoutes.post("/addPost", (c) => userPostController.postUser(c));
 
 userPostRoutes.get("/getUserPosts/:userId?", (c) =>
   userPostController.getUserPosts(c)
